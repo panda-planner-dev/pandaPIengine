70d85e073c03e6927f0f8c5d59a3d05d21e87a05
Daniel Höller <hoeller@cs.uni-saarland.de>
2020-11-03 21:58:12 +0100

    auto format code

--- src/heuristics/rcHeuristics/hsAddFF.h
+++ src/heuristics/rcHeuristics/hsAddFF.h
@@ -20,47 +20,51 @@ using namespace std;
 
 namespace progression {
 
-enum myHeu {
-	sasAdd, sasFF
-};
+    enum myHeu {
+        sasAdd, sasFF
+    };
 
-class ComparePair {
-public:
-	bool operator()(pair<int, int>* n1, pair<int, int>* n2);
-};
+    class ComparePair {
+    public:
+        bool operator()(pair<int, int> *n1, pair<int, int> *n2);
+    };
 
-class hsAddFF {
-public:
-	hsAddFF(Model* sas);
-	virtual ~hsAddFF();
-#if (STATEREP == SRCALC1) ||(STATEREP == SRCOPY)
-	int getHeuristicValue(bucketSet& s, noDelIntSet& g);
-#elif (STATEREP == SRCALC2) 
-	int getHeuristicValue(noDelIntSet& s, noDelIntSet& g);
+    class hsAddFF {
+    public:
+        hsAddFF(Model *sas);
+
+        virtual ~hsAddFF();
+
+#if (STATEREP == SRCALC1) || (STATEREP == SRCOPY)
+
+        int getHeuristicValue(bucketSet &s, noDelIntSet &g);
+
+#elif (STATEREP == SRCALC2)
+        int getHeuristicValue(noDelIntSet& s, noDelIntSet& g);
 #elif(STATEREP == SRLIST)
-	int getHeuristicValue(noDelIntSet& s, noDelIntSet& g);
+        int getHeuristicValue(noDelIntSet& s, noDelIntSet& g);
 #endif
-	Model* m;
-	myHeu heuristic = sasFF;
-	int calls = 0;
-private:
-	// todo: when parallelized, this must be per core
-	IntPairHeap* queue;
-	int* hValPropInit;
+        Model *m;
+        myHeu heuristic = sasFF;
+        int calls = 0;
+    private:
+        // todo: when parallelized, this must be per core
+        IntPairHeap *queue;
+        int *hValPropInit;
 
-	int* numSatPrecs;
-	int* hValOp;
-	int* hValProp;
-	int* reachedBy;
+        int *numSatPrecs;
+        int *hValOp;
+        int *hValProp;
+        int *reachedBy;
 
-	noDelIntSet markedFs;
-	noDelIntSet markedOps;
-	IntStack needToMark;
+        noDelIntSet markedFs;
+        noDelIntSet markedOps;
+        IntStack needToMark;
 
-	bool allActionsCostOne = false;
+        bool allActionsCostOne = false;
 
-	int getFF(noDelIntSet& g, int hVal);
-};
+        int getFF(noDelIntSet &g, int hVal);
+    };
 
 } /* namespace progression */
 
