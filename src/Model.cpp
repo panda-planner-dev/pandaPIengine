01806cc3a94ec3dc044844bad0ca2cd08e7af46a
florian-arch <florian@pollitt.de>
2021-03-13 12:26:10 +0100

    bugfix sas+

--- src/Model.cpp
+++ src/Model.cpp
@@ -3111,7 +3111,7 @@ void Model::calcMinimalImpliedX() {
     }
     factStrsSP = new string[numStateBitsSP];
     for (int i = 0; i < numVars; i++){
-      for (int j = 0; j < firstIndex[i] - lastIndex[i] + 1; j++){
+      for (int j = 0; j < lastindex[i] - firstIndex[i] + 1; j++){
         bitsToSP[firstIndex[i] + j] = firstIndex[i] + j + sasPlusOffset[i];
         factStrsSP[firstIndex[i] + j + sasPlusOffset[i]] = factStrs[firstIndex[i] + j];
       }
