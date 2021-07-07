2044da02d4a78fc77f588ca68d66fadd85e69646
Pascal Bercher <pascal.bercher@anu.edu.au>
2021-07-07 17:27:14 +1000

    update due to change in required parser input

--- problemSolver.sh
+++ problemSolver.sh
@@ -2,6 +2,6 @@
 
 #author: Pascal Bercher (pascal.bercher@anu.edu.au)
 
-./pandaPIparser $1 $2 > problem$3.parsed
+./pandaPIparser $1 $2 problem$3.parsed > problem$3.parsed.log
 ./pandaPIgrounder problem$3.parsed problem$3.sas 2> problem$3.stderr.statistics > problem$3.stdout.statistics
 ./pandaPIengine problem$3.sas > problem$3.solution
