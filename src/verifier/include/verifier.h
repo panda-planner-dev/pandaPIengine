f64d17ca986bbd01d84b48674e9eb1a26499cb48
Songtuan Lin <songtuan.lin@anu.edu.au>
2023-01-09 15:16:35 +1100

    to-verifier

---
+++ src/verifier/include/verifier.h
@@ -0,0 +1,77 @@
+#include "Model.h"
+#include <fstream>
+#include <string.h>
+#include <unordered_map>
+#include <algorithm>
+
+class Verifier {
+    public:
+        Verifier(string htnFile, string planFile) {
+            this->readHTNFile(htnFile);
+            vector<string> planStr = this->readPlanFile(planFile);
+            this->plan = parsePlan(planStr);
+        }
+
+        bool getResult() {return this->result;}
+
+    protected:
+        Model *htn;
+        vector<int> plan;
+        bool result;
+
+    private:
+        void readHTNFile(string htnFile) {
+            cout << "read model from" << htnFile << "\n";
+            std::ifstream *fileInput = new std::ifstream(htnFile);
+            if(!fileInput->good()) {
+                std::cerr << "Unable to open input file " << htnFile << ": " << strerror (errno) << std::endl;
+                exit(-1);
+            }
+            std::istream * inputStream;
+            inputStream = fileInput;
+
+            bool useTaskHash = true;
+            /* Read model */
+            // todo: the correct value of maintainTaskRechability depends on the heuristic
+            eMaintainTaskReachability reachability = mtrALL;
+            bool trackContainedTasks = useTaskHash;
+            this->htn = new Model(trackContainedTasks, reachability, true, true);
+            this->htn->filename = htnFile;
+            this->htn->read(inputStream);
+            cout << "reading file completed" << endl;
+        }
+
+        vector<int> parsePlan(vector<string> planStr) {
+            vector<int> planNum;
+            unordered_map<string, int> taskToIndex;
+            for (int i = 0; i < this->htn->numTasks; i++) {
+                string taskName = this->htn->taskNames[i];
+                std::transform(taskName.begin(), taskName.end(), taskName.begin(), ::tolower);
+                taskToIndex.insert({taskName, i});
+            }
+            for (int i = 0; i < plan.size(); i++) {
+                if (!taskToIndex.count(planStr[i])) {
+                    std::cerr << "Plan contains unreachable actions, plan is not a solution" << endl;
+                    exit(-1);
+                }
+                planNum.push_back(taskToIndex[planStr[i]]);
+            }
+            return planNum;
+        }
+
+        vector<string> readPlanFile(string planFile) {
+            vector<string> plan;
+            ifstream fin(planFile);
+            if(!fin.good()) {
+                std::cerr << "Unable to open input file " << planFile << ": " << strerror (errno) << std::endl;
+                exit(-1);
+            }
+            string action;
+            while(std::getline(fin, action, ';')) {
+                action.erase(std::remove(action.begin(), action.end(), '\n'), action.end());
+                std::transform(action.begin(), action.end(), action.begin(), ::tolower);
+                plan.push_back(action);
+            }
+            return plan;
+        }
+};
---
+++ src/verifier/util/include/execution.h
@@ -0,0 +1,20 @@
+#ifndef _execution_inc_h_
+#define _execution_inc_h_
+
+#include "Model.h"
+#include <boost/dynamic_bitset.hpp>
+
+class PlanExecution {
+    public:
+        PlanExecution(Model *htn, vector<int> plan);
+        unordered_set<int> getState(int pos) {return this->stateSeq[pos];}
+        boost::dynamic_bitset<> getStateBits(int pos) {return stateSeqBits[pos];}
+        bool isPropTrue(int pos, int prop) {return stateSeq[pos].count(prop);}
+        bool isExecutable() {return executable;}
+
+    private:
+        bool executable;
+        vector<unordered_set<int>> stateSeq;
+        vector<boost::dynamic_bitset<>> stateSeqBits;
+};
+#endif
